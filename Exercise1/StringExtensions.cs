c019d09bad0e723b8ae4a2cfe14dc60f5affa99f
bfawber <bfawber@gmail.com>
2017-06-24 22:07:40 -0400

    Initial Commit

---
+++ Exercise1/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise1
+{
+    public static class StringExtensions
+    {
+		/**
+		 * Exercise: 
+		 * Create a string extension metbod called MuffinReverse to return the string reversed. 
+		 * Example: if you have:
+		 * string str = "bryan";
+		 * when you call str.MuffinReverse() it should return "nayrb"
+		 * 
+		 * Extension Methods Documentation:
+		 * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
+		 * 
+		 * Then properly test it using Unit Tests
+		 * 
+		 * WHEN FINISHED CREATE A NEW BRANCH IN GITHUB, PUSH IT, AND CREATE A PULL REQUEST TO MASTER
+		 * Resource for github:
+		 * https://try.github.io/levels/1/challenges/1
+		 */
+
+
+		/// <summary>
+		/// Example extension method that will return the last character of a string.
+		/// This can be called using any string variable. Example:
+		/// string str = "hello world";
+		/// char lastChar = str.LastChar();
+		/// </summary>
+		/// <param name="str">The string to get the last character from</param>
+		/// <returns>The last character in the string</returns>
+		public static char LastChar(this string str)
+		{
+			return str[str.Length - 1];
+		}
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("01329cfa-ef36-463a-961c-af9b8d134f99")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
